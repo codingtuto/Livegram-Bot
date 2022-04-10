04111c25c524913f7668a80c112cf390e9a6bb77
Coding Team <87824092+codingtuto@users.noreply.github.com>
2022-04-10 14:09:10 +0000

    fonctionnel pour telegram X

--- bot/plugins/un_ban_user.py
+++ bot/plugins/un_ban_user.py
@@ -73,5 +73,5 @@ async def un_ban_command(client: Client, message: Message):
     await message.reply_text(
         f"<a href='tg://user?id={user_id}'>"
         "user"
-        "</a> <b>unbanned</b>."
+        "</a> <b>débanni</b>."
     )
